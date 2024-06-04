4195264e19ef85d44a3695c3328ee0b6f29d5350
[Mrinmoy <mvmkvn19211@gmail.com>
2024-06-04 22:12:09 +0530

    first commit

---
+++ a1.js
@@ -0,0 +1,38 @@
+'use-strict';
+
+
+// Constructor function
+// It is called using a new operatoe
+
+//Cons Function
+const Person = function(firstName,birthYear) {
+    console.log(`We have ${firstName} and ${birthYear}`)
+}
+
+
+
+new Person("Jonas",2025)
+
+// We call this using a new operator
+
+
+
+//Steps
+/**
+ * 
+ * // We are calling it using New operator
+ * New {} emp obj is created
+ * function is called  ,  this = {}
+ * {} Linked ot the prototype
+ * 
+ */
+
+/**
+ * Prototypal Inheritence
+ * 
+ * 
+ * Prot Methods
+ *  
+ * 
+ * Obj  Cn access methods
+ */
